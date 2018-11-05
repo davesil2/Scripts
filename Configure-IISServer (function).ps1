81848e955b70846149e4c8d62bb871119a9884f9
D Silberhorn <midlandds@midlandsb.com>
2018-11-05 16:43:34 -0600

    Updated commands for Role install and path variable for configuration

--- Configure-IISServer (function).ps1
+++ Configure-IISServer (function).ps1
@@ -55,6 +55,7 @@ function Configure-IISServer
         [String[]]$RolesandFeatures = ('Web-Server','Web-Common-Http','Web-Default-Doc','Web-Dir-Browsing','Web-Http-Errors','Web-Static-Content','Web-Health','Web-http-logging','Web-custom-logging','web-http-tracing','web-performance','web-stat-compression','web-dyn-compression','web-security','web-filtering','web-basic-auth','web-ip-security','web-url-auth','web-windows-auth','web-app-dev','web-net-ext45','web-appinit','web-asp','web-asp-net45','web-isapi-ext','web-isapi-filter','web-mgmt-console','web-mgmt-service','Web-Log-Libraries','Web-Request-Monitor','Web-Digest-Auth','Web-Mgmt-Compat','Web-Metabase','Web-Lgcy-Scripting','Web-WMI')
     )
 
+    $ErrorActionPreference = 'Stop'
     #region Validate Variables
     ##Test Server Exists
     try {
@@ -98,11 +99,8 @@ function Configure-IISServer
     try {
         $result = $null
         Write-Verbose ('Starting Roles and Features Install...')
-        $Script = {
-            Add-WindowsFeature $RolesandFeatures
-        }
-        $result = Invoke-Command -ComputerName $ServerName -ScriptBlock $Script  
-        Write-Verbose ('Windows Features {0} Installed on {1}' -f $RolesandFeatures,$ServerName)  
+        $RolesandFeatures | Add-WindowsFeature -ComputerName $ServerName -Credential $AdminCreds | Out-Null
+        Write-Verbose ('Windows Features {0} Installed on {1}' -f ($RolesandFeatures -join ','),$ServerName)  
     }
     catch {
         throw ('Problem Installing IIS Features {0} on server {1}' -f $RolesandFeatures,$ServerName)
@@ -133,23 +131,23 @@ function Configure-IISServer
             }
     
             ##move source lcoation for IIS files
-            Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name "traceFailedRequestsLogging.Directory" -Value "$RootDriveLetter:\InetPub\Logs\FailedRequestLogFiles"
-            Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name "LogFile.Directory" -Value "$RootDriveLetter:\InetPub\Logs\LogFiles"
-            Set-WebConfigurationProperty "/system.applicationHost/log" -name "centralBinaryLogFile.directory" -Value "$RootDriveLetter:\InetPub\Logs\LogFiles"
-            Set-WebConfigurationProperty "/system.applicationHost/log" -name "centralW3CLogFile.directory" -Value "$RootDriveLetter:\InetPub\Logs\LogFiles"
-            Set-WebConfigurationProperty "/system.applicationHost/configHistory" -name "Path" -Value "$RootDriveLetter:\InetPub\History"
-            Set-WebConfigurationProperty "/system.webServer/asp" -name "cache.disktemplateCacheDirectory" -Value "$RootDriveLetter:\InetPub\Temp\ASP Compiled Templates"
-            Set-WebConfigurationProperty "/system.webServer/httpCompression" -name "directory" -Value "$RootDriveLetter:\InetPub\Temp\IIS Temporary Compressed Files"
-            Set-ItemProperty 'IIS:\Sites\Default Web Site' -Name 'PhysicalPath' -Value "$RootDriveLetter:\InetPub\WWWRoot"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='401']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='403']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='404']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='405']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='406']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='412']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='500']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='501']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
-            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='502']" -name "prefixLanguageFilePath" -Value "$RootDriveLetter:\InetPub\CustErr"
+            Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name "traceFailedRequestsLogging.Directory" -Value ("{0}:\InetPub\Logs\FailedRequestLogFiles" -f $RootDriveLetter)
+            Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name "LogFile.Directory" -Value ("{0}:\InetPub\Logs\LogFiles" -f $RootDriveLetter)
+            Set-WebConfigurationProperty "/system.applicationHost/log" -name "centralBinaryLogFile.directory" -Value ("{0}:\InetPub\Logs\LogFiles" -f $RootDriveLetter)
+            Set-WebConfigurationProperty "/system.applicationHost/log" -name "centralW3CLogFile.directory" -Value ("{0}:\InetPub\Logs\LogFiles" -f $RootDriveLetter)
+            Set-WebConfigurationProperty "/system.applicationHost/configHistory" -name "Path" -Value ("{0}:\InetPub\History" -f $RootDriveLetter)
+            Set-WebConfigurationProperty "/system.webServer/asp" -name "cache.disktemplateCacheDirectory" -Value ("{0}:\InetPub\Temp\ASP Compiled Templates" -f $RootDriveLetter)
+            Set-WebConfigurationProperty "/system.webServer/httpCompression" -name "directory" -Value ("{0}:\InetPub\Temp\IIS Temporary Compressed Files" -f $RootDriveLetter)
+            Set-ItemProperty 'IIS:\Sites\Default Web Site' -Name 'PhysicalPath' -Value ("{0}:\InetPub\WWWRoot" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='401']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='403']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='404']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='405']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='406']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='412']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='500']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='501']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
+            Set-WebConfigurationProperty  "/system.WebServer/HttpErrors/error[@statusCode='502']" -name "prefixLanguageFilePath" -Value ("{0}:\InetPub\CustErr" -f $RootDriveLetter)
             Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\InetStp -Name PathWWWRoot -Value '$RootDriveLetter:\InetPub\wwwroot' -Force
             Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\InetStp -Name PathFTPRoot -Value '$RootDriveLetter:\InetPub\wwwroot' -Force
             Set-ItemProperty -Path HKLM:\system\CurrentControlSet\Services\was\Parameters -Name ConfigIsolationPath -Value '$RootDriveLetter:\InetPub\temp\AppPools' -Force
@@ -176,7 +174,7 @@ function Configure-IISServer
             $Script = {
                 Stop-Service wmsvc -force
                 Write-Verbose ('Updating Certificate for Web Mgmt Service')
-                $cert = gci Cert:\LocalMachine\My | ?{$_.subject -like "*CN=" + $env:COMPUTERNAME + "*"}
+                $cert = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.subject -like "*CN=" + $env:COMPUTERNAME + "*"}
                 if ($cert)
                 {
                     Write-Verbose ('Found Certificate to replace self signed')
