db97547dfff6b8fa59f98429aa6bf0efabe2b47d
Silberhorn <David.Silberhorn@boncura.com>
2020-01-21 10:49:12 -0600

    Update Fix File Sizes to keep job output and fail job if error

--- SQLJobs/Fix File Sizes.ps1
+++ SQLJobs/Fix File Sizes.ps1
@@ -1,27 +1,30 @@
 # Set Variables
 $_Instance = "$(ESCAPE_DQUOTE(SRVR))"
 $LargeGrowth = 10240000
+$_JobSucceded = $true
+$_Result = @()
 
 # fix Instance if default
 if ($_Instance -notlike '*\*') {
-    ('Using Default Instance...')
+    $_Result += ('Using Default Instance...')
     $_Instance += "\Default"
 }
 
 # Connect to SQL Instance
-('Connecting to "{0}"' -f $_Instance)
+$_Result += ('Connecting to "{0}"' -f $_Instance)
 $_SQL = Get-Item "SQLServer:\SQL\$_Instance"
 
 try {
-    ('Getting Databases from [{0}]' -f $_Instance)
+    $_Result += ('Getting Databases from [{0}]' -f $_Instance)
     $_dbs = Get-ChildItem ("SQLSERVER:\SQL\$_instance\Databases") -Force | Where-Object {$_.Status -eq 'Normal' -and $_.Name -ne 'TempDB' -and $_.name -ne 'Model'}
 } catch {
-    throw ('Error getting databases from server: {0}' -f $error[0].message)
+    $_Result += ('Error getting databases from server: {0}' -f $error[0].message)
+    $_JobSucceded = $false
 }
 
 # Filter DB's that can't be backed up
 if ($_SQL.isClustered) {
-    ("Filtering Clustered DB's that are not Primary...")
+    $_Result += ("Filtering Clustered DB's that are not Primary...")
     $_dbs = $_dbs | Where-Object {!($_.AvailabilityGroupName) -or ($SQLServer.AvailabilityGroups[$_.availabilityGroupName].LocalReplicaRole -eq 'Primary')}
 }
 
@@ -39,19 +42,38 @@ foreach ($_db in $_dbs) {
             $_file.growth = $GrowthSize
             $_file.GrowthType = 'KB'
 
-            $_file.Alter()
-            $_file.refresh()
+            try {
+                $_file.Alter()
+                $_File.refresh()
 
-            ('Database [{0}] : File [{1}] (GrowthSize [{2}])' -f $_db.name,$_file.FileName,$GrowthSize)
+                $_Result += ('Database [{0}] : File [{1}] - (GrowthSize [{2}])' -f $_db.name,$_file.FileName,$GrowthSize)
+            } catch {
+                $_Result += ('Database [{0}] : File [{1}] - Growth Size Set Failure!' -f $_db.name,$_file.FileName,$GrowthSize)
+                $_JobSucceded = $false
+            }
+            
         }
 
         if ($_file.size/$GrowthSize -isnot [int]) {
             $_filesize = $_logfile.Size
             $_file.Size = ([math]::Truncate($_file.size / $GrowthSize) + 1) * $GrowthSize
-            $_file.alter()
-            $_file.refresh()
 
-            ('Database [{0}] : File [{1}] (File Size [{2}]) (Previous Size [{3}])' -f $_db.name,$_file.FileName,$_file.size,$_FileSize)
+            try {
+                $_file.alter()
+                $_file.refresh()
+    
+                $_Result += ('Database [{0}] : File [{1}] - (File Size [{2}]) (Previous Size [{3}])' -f $_db.name,$_file.FileName,$_file.size,$_FileSize)
+            } catch {
+                $_Result += ('Database [{0}] : File [{1}] - Resize Failure!' -f $_db.name,$_file.FileName)
+                $_JobSucceded = $false
+            }
+            
         }
     }
+}
+
+if (-Not $_JobSucceded) {
+    Write-Error ($_Result | out-string) -ErrorAction Stop
+} else {
+    Write-Output ($_Result | Out-string)
 }
