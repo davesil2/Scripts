5fc9991a41a456f67fb9bd99cf0d052a3f26fd73
Silberhorn <David.Silberhorn@boncura.com>
2020-01-21 10:48:26 -0600

    Update Indexes to provide output and error to job results

--- SQLJobs/Indexes.ps1
+++ SQLJobs/Indexes.ps1
@@ -2,27 +2,30 @@
 $_Instance = "$(ESCAPE_DQUOTE(SRVR))"
 $rebuildfragmentationlevel = 30
 $reorganizefragmentationlevel = 5
+$_JobSucceded = $true
+$_Result = @()
 
 # fix Instance if default
 if ($_Instance -notlike '*\*') {
-    ('Using Default Instance...')
+    $_Result += ('Using Default Instance...')
     $_Instance += "\Default"
 }
 
 # Connect to SQL Instance
-('Connecting to "{0}"' -f $_Instance)
+$_Result += ('Connecting to "{0}"' -f $_Instance)
 $_SQL = Get-Item "SQLServer:\SQL\$_Instance"
 
 try {
-    ('Getting Databases from [{0}]' -f $_Instance)
+    $_Result += ('Getting Databases from [{0}]' -f $_Instance)
     $_dbs = Get-ChildItem ("SQLSERVER:\SQL\$_instance\Databases") -Force | Where-Object {$_.Status -eq 'Normal' -and $_.Name -ne 'TempDB' -and $_.name -ne 'Model'}
 } catch {
-    throw ('Error getting databases from server: {0}' -f $error[0].message)
+    $_Result +=  ('Error getting databases from server: {0}' -f $error[0].message)
+    $_JobSucceded = $false
 }
 
 # Filter DB's that can't be backed up
 if ($_SQL.isClustered) {
-    ("Filtering Clustered DB's that are not Primary...")
+    $_Result += ("Filtering Clustered DB's that are not Primary...")
     $_dbs = $_dbs | Where-Object {!($_.AvailabilityGroupName) -or ($SQLServer.AvailabilityGroups[$_.availabilityGroupName].LocalReplicaRole -eq 'Primary')}
 }
 
@@ -38,25 +41,33 @@ foreach ($_db in $_dbs) {
                         $_index.RebuildAllIndexes()
                         $_index.Alter()
 
-                        ('Database [{0}] : Index [{1}] - Fragmentation [{2}] - Rebulid Success!' -f $_db.Name,$_index.Name,[int]$_frag.averagefragmentation)
+                        $_Result += ('Database [{0}] : Index [{1}] - Fragmentation [{2}] - Rebulid Success!' -f $_db.Name,$_index.Name,[int]$_frag.averagefragmentation)
                     } catch {
-                        ('Database [{0}] : Index [{1}] - Fragmentation [{2}] - Rebulid Failed!' -f $_db.Name,$_index.Name,[int]$_frag.averagefragmentation)
+                        $_Result += ('Database [{0}] : Index [{1}] - Fragmentation [{2}] - Rebulid Failed!' -f $_db.Name,$_index.Name,[int]$_frag.averagefragmentation)
+                        $_JobSucceded = $false
                     }
                 } elseif ($_frag.averagefragmentation -gt $reorganizefragmentationlevel) {
                     try {
                         $_index.ReorganizeAllIndexes()
                         $_index.Alter()
 
-                        ('Database [{0}] : Index [{1}] - Fragmentation [{2]} - Reorganize Success!' -f $_db.name,$_index.name,[int]$_frag.averagefragmentation)
+                        $_Result += ('Database [{0}] : Index [{1}] - Fragmentation [{2]} - Reorganize Success!' -f $_db.name,$_index.name,[int]$_frag.averagefragmentation)
                     } catch {
-                        ('Database [{0}] : Index [{1}] - Fragmentation [{2]} - Reorganize Failed!' -f $_db.name,$_index.name,[int]$_frag.averagefragmentation)
+                        $_Result += ('Database [{0}] : Index [{1}] - Fragmentation [{2]} - Reorganize Failed!' -f $_db.name,$_index.name,[int]$_frag.averagefragmentation)
+                        $_JobSucceded = $false
                     }
                 } else {
-                    ('Database [{0}] : Index [{1}] - Fragmentation [{2}] - Fragmentation Not High enough!' -f $_db.name,$_index.name,[int]$_frag.averagefragmentation)
+                    $_Result += ('Database [{0}] : Index [{1}] - Fragmentation [{2}] - Fragmentation Not High enough!' -f $_db.name,$_index.name,[int]$_frag.averagefragmentation)
                 }
 
                 $_index.Refresh()
             }
         }
     }
+}
+
+if (-Not $_JobSucceded) {
+    Write-Error ($_Result | out-string) -ErrorAction Stop
+} else {
+    Write-Output ($_Result | Out-string)
 }
