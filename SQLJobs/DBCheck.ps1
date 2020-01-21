139a4f7a19199de8454899723af536d21fe3204e
Silberhorn <David.Silberhorn@boncura.com>
2020-01-21 10:47:46 -0600

    Update DB Check to error when failed and provide output

--- SQLJobs/DBCheck.ps1
+++ SQLJobs/DBCheck.ps1
@@ -2,26 +2,29 @@
 $_Instance = "$(ESCAPE_DQUOTE(SRVR))"
 $_JobSucceded = $true
 
+$_Result = @()
+
 # fix Instance if default
 if ($_Instance -notlike '*\*') {
-    ('Using Default Instance...')
+    $_Result += ('Using Default Instance...')
     $_Instance += "\Default"
 }
 
 # Connect to SQL Instance
-('Connecting to "{0}"' -f $_Instance)
+$_Result += ('Connecting to "{0}"' -f $_Instance)
 $_SQL = Get-Item "SQLServer:\SQL\$_Instance"
 
 try {
-    ('Getting Databases from [{0}]' -f $_Instance)
+    $_Result += ('Getting Databases from [{0}]' -f $_Instance)
     $_dbs = Get-ChildItem ("SQLSERVER:\SQL\$_instance\Databases") -Force | Where-Object {$_.Status -eq 'Normal' -and $_.Name -ne 'TempDB' -and $_.name -ne 'Model'}
 } catch {
-    throw ('Error getting databases from server: {0}' -f $error[0].message)
+    $_Result += ('Error getting databases from server: {0}' -f $error[0].message)
+    $_JobSucceded = $false
 }
 
 # Filter DB's that can't be backed up
 if ($_SQL.isClustered) {
-    ("Filtering Clustered DB's that are not Primary...")
+    $_Result += ("Filtering Clustered DB's that are not Primary...")
     $_dbs = $_dbs | Where-Object {!($_.AvailabilityGroupName) -or ($SQLServer.AvailabilityGroups[$_.availabilityGroupName].LocalReplicaRole -eq 'Primary')}
 }
 
@@ -29,14 +32,16 @@ foreach ($_db in $_dbs) {
     if ($_db -and $_db.tables) {
         try {
             $_db.CheckTables('None')
-            ('Database [{0}] - Check Completed.' -f $_db.name)
+            $_Result += ('Database [{0}] - Check Completed.' -f $_db.name)
         } catch {
-            ('Database [{0}] - Check Failed!' -f $_db.name)
+            $_Result += ('Database [{0}] - Check Failed!' -f $_db.name)
             $_JobSucceded = $false
         }
     }
 }
 
 if (-Not $_JobSucceded) {
-    throw ('Job Failed, see output...')
+    Write-Error ($_Result | out-string) -ErrorAction Stop
+} else {
+    Write-Output ($_result | Out-string)
 }
