f0d08eee9f0f68951e0df8e90d42440eed185e56
David Silberhorn <davesil2@hotmail.com>
2025-02-11 21:55:35 -0600

    add get-citrixmachinecatalog to citrix functions

--- Citrix Functions.ps1
+++ Citrix Functions.ps1
@@ -1,42 +1,35 @@
 <#
-    .SYNOPSIS
-    
-    Initialize connection to citrix cloud and obtain header for future requests
+.SYNOPSIS
+Initialize connection to citrix cloud and obtain header for future requests
 
-    .DESCRIPTION
+.DESCRIPTION
+This function will call to Citrix Cloud REST API and obtain a token and generate appropriate headers for other function calls
 
-    This function will call to Citrix Cloud REST API and obtain a token and generate appropriate headers for other function calls
+.PARAMETER CloudURI
+CloudURI is the URI that will be use for the FQDN of the call the citrix cloud.
 
-    .PARAMETER CloudURI
+    DEFAULT: api-us.cloud.com
 
-    CloudURI is the URI that will be use for the FQDN of the call the citrix cloud.
+    https://developer-docs.citrix.com/en-us/citrix-cloud/citrix-cloud-api-overview/get-started-with-citrix-cloud-apis
 
-        DEFAULT: api-us.cloud.com
+.PARAMETER ClientID
+ClientID is the generated ID for the Service Principal or API Key Pair based on direction from the Citrix Developer Guid
 
-        https://developer-docs.citrix.com/en-us/citrix-cloud/citrix-cloud-api-overview/get-started-with-citrix-cloud-apis
+.PARAMETER ClientSecret
+ClientSecret is like the pass phrase associated with the ClientID, this should be protected
 
-    .PARAMETER ClientID
-    
-    ClientID is the generated ID for the Service Principal or API Key Pair based on direction from the Citrix Developer Guid
+.PARAMETER CustomerID
+CustomerID is the unique accounts.cloud.com ID provided by Citrix for your unique tenant space
 
-    .PARAMETER ClientSecret
+.EXAMPLE
+Calling the function with the specifice ClientID, ClientSecret and CustomerID will return headers to be used in future function calls
 
-    ClientSecret is like the pass phrase associated with the ClientID, this should be protected
+$Headers = Connect-CitrixRESTAPI `
+    -ClientID       'abdf26c6-39b1-41cf-af29-b3dfc08c1580' `
+    -ClientSecret   'asdf943sadfj342ad==' `
+    -CustomerID     'u9tviazeowms'
 
-    .PARAMETER CustomerID
-
-    CustomerID is the unique accounts.cloud.com ID provided by Citrix for your unique tenant space
-
-    .EXAMPLE
-
-    Calling the function with the specifice ClientID, ClientSecret and CustomerID will return headers to be used in future function calls
-
-    $Headers = Connect-CitrixRESTAPI `
-        -ClientID       'abdf26c6-39b1-41cf-af29-b3dfc08c1580' `
-        -ClientSecret   'asdf943sadfj342ad==' `
-        -CustomerID     'u9tviazeowms'
-    
-        **NOTE: the headers variable would be used by other functions
+    **NOTE: the headers variable would be used by other functions
 #>
 function Connect-CitrixRESTAPI {
     Param(
@@ -112,4 +105,68 @@ function Connect-CitrixRESTAPI {
         throw 'Error Occured - unable to obtain token, verify credentials'
     }
     
+}
+
+<#
+.SYNOPSIS
+Obtain a list of Machine Catalogs from Citrix Cloud
+
+.DESCRIPTION
+Using headers generted from Connect-CitrixRESTAPI or manually, the function calls the the default FQDN api-us.cloud.com to get a list of machine catalogs present in the Citrix Cloud Console
+
+.PARAMETER CloudURI
+CloudURI is the URI that will be use for the FQDN of the call the citrix cloud.
+
+    DEFAULT: api-us.cloud.com
+
+    https://developer-docs.citrix.com/en-us/citrix-cloud/citrix-cloud-api-overview/get-started-with-citrix-cloud-apis
+
+.PARAMETER Headers
+Header information used to authenticate and authorize access into the cloud platform.  The header names below are required.
+
+    Citrix-CustomerID
+    CitrixInstanceID
+    Authorization
+
+.EXAMPLE
+Calling to Citrix Cloud requires a valid headers output.  Then calling the Get-CitrixMachineCatalog with that information will provide a list of catalogs
+
+$Headers = Connect-CitrixRESTAPI `
+    -ClientID $env:clientid `
+    -ClientSecret $env:clientsecret `
+    -CustomerID $env:customerid
+
+Get-CitrixMachineCatalog `
+    -Headers $Headers
+
+#>
+function Get-CitrixMachineCatalog {
+    Param(
+        [string]$CloudURI = 'api-us.cloud.com',
+        [Parameter(Mandatory=$true)]
+        [hashtable]$Headers
+    )
+
+    # build URI for use to access machine catalog
+    [System.UriBuilder]$URI = ('https://{0}/cvad/manage/MachineCatalogs' -f $CloudURI)
+
+    # build parameters for splat into invoke-restmethod
+    $parameters = @{
+        ContentType = 'application/json'
+        UserAgent   = 'Mozilla/5.0'
+        ErrorAction = 'silentlyContinue'
+        Uri         = $URI.Uri
+        Headers     = $Headers
+    }
+
+    # execute call to uri with Splat parameters
+    $_response = Invoke-RestMethod @parameters
+
+    if ($_response) {
+        # return list of machine catalogs if the return is valid
+        return $_response.items
+    } else {
+        # throw an error if no output is created
+        throw 'ERROR: no response recieved, check header output, credentials and customerid'
+    }
 }
