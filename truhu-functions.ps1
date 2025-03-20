4b1944117a6ee01516ab67da0ba51d92e3513f07
David Silberhorn <davesil2@hotmail.com>
2025-03-20 14:22:15 -0500

    new function for add and clear users

--- truhu-functions.ps1
+++ truhu-functions.ps1
@@ -29,10 +29,11 @@ function Get-TruHutoken {
     }
 
     $Parameters = @{
-        Method  = 'POST'
-        URI     = $URI.Uri
-        Body    = $tokenbody
-        Header  = $headers
+        Method      = 'POST'
+        URI         = $URI.Uri
+        Body        = $tokenbody
+        Header      = $headers
+        ErrorAction = 'silentlyContinue'
     }
 
     $token = Invoke-RestMethod @Parameters
@@ -72,9 +73,11 @@ function Get-TruHuEmployees {
     $URI.Query = $Query.ToString()
 
     $Parameters = @{
-        Method = 'GET'
-        Header = $Headers
-        URI = $URI.Uri
+        Method      = 'GET'
+        Header      = $Headers
+        URI         = $URI.Uri
+        ErrorAction = 'silentlyContinue'
+        ContentType = 'application/json'
     }
 
     $Employees = Invoke-RestMethod @Parameters
@@ -86,38 +89,210 @@ function Get-TruHuEmployees {
     }
 }
 
-function Update-TruHuEmployees {
+function Add-TruHuEmployee {
     Param(
-        [String]$FQDN = 'api.truhu.com',
+        [Parameter(Mandatory=$true)]
         $APIToken,
-        $Census
+        [Parameter(Mandatory=$false)]
+        [String]$FQDN = 'api.truhu.com',
+        [Parameter(Mandatory=$true)]
+        [String]$CompanyID,
+        [Parameter(Mandatory=$true)]
+        [String]$FirstName,
+        [Parameter(Mandatory=$true)]
+        [String]$LastName,
+        [Parameter(Mandatory=$false)]
+        [String]$MiddleInitial,
+        [Parameter(Mandatory=$true)]
+        [String]$EmailAddress,
+        [Parameter(Mandatory=$true)]
+        [String]$CellPhone,
+        [Parameter(Mandatory=$false)]
+        [String[]]$Locations = @(),
+        [Parameter(Mandatory=$false)]
+        [String[]]$Departments = @(),
+        [Parameter(Mandatory=$false)]
+        [String]$Classification,
+        [Parameter(Mandatory=$false)]
+        [String]$Status,
+        [Parameter(Mandatory=$false)]
+        [String]$JobTitle,
+        [Parameter(Mandatory=$false)]
+        [String]$Language,
+        [Parameter(Mandatory=$false)]
+        [String]$EmployeeId,
+        [Parameter(Mandatory=$false)]
+        [String]$TimeZone,
+        [Parameter(Mandatory=$false)]
+        [hashtable]$ManagerDetails = @{EmailAddress=$null},
+        [Parameter(Mandatory=$false)]
+        [String]$DateOfHire,
+        [Parameter(Mandatory=$false)]
+        [hashtable]$TerminationDetails = @{Date=$null;Type=$null},
+        [Parameter(Mandatory=$false)]
+        [String]$DateOfBirth,
+        [Parameter(Mandatory=$false)]
+        [String]$Gender,
+        [Parameter(Mandatory=$false)]
+        [String]$Ethnicity,
+        [Parameter(Mandatory=$false)]
+        [string]$RemoteID,
+        [Parameter(Mandatory=$false)]
+        [hashtable]$Address = @{address1='n/a';city='n/a';state='n/a';zip='n/a'},
+        [ValidateScript(
+            {$_ -in ([enum]::GetNames([net.securityprotocoltype]))},
+            ErrorMessage = 'ERROR: TLS version must be supported on system (run [enum]::GetNames([net.securityprotocoltype]) for a valid list)'
+        )]
+        [string]$TLSVersion = 'Tls12'
     )
 
     # set TLS version
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::"$TLSVersion"
     
     # generate uri for generating token
-    [System.UriBuilder]$URI = ('https://{0}/Census/Update' -f $FQDN)
+    [System.UriBuilder]$URI = ('https://{0}/Census' -f $FQDN)
 
     $Headers = @{
         Authorization   = ('Bearer {0}' -f $APIToken.access_token)
         Accept          = 'application/json'
     }
 
-    $Body = @{
-        cencus = $Census
+    if (-Not $RemoteID) {
+        $RemoteID = $EmailAddress
+    }
+
+    $Employee = @{
+        FirstName = $FirstName
+        LastName = $LastName
+        MiddleInitial = $MiddleInitial
+        EmailAddress = $EmailAddress
+        CellPhone = $CellPhone
+        Locations = $Locations
+        Departments = $Departments
+        Classification = $Classification
+        Status = $Status
+        JobTitle = $JobTitle
+        Language = $Language
+        EmployeeId = $EmployeeId
+        TimeZone = $TimeZone
+        ManagerDetails = $ManagerDetails
+        DateOfHire = $DateOfHire
+        TerminationDetails = $TerminationDetails
+        DateOfBirth = $DateOfBirth
+        Gender = $Gender
+        Ethnicity = $Ethnicity
+        Address = $Address
+        remoteID = $remoteID
+    }
+
+    $Census = Get-TruHuEmployees `
+        -FQDN $FQDN `
+        -CompanyID $CompanyID `
+        -APIToken $APIToken
+
+    $Census.Employees += $Employee
+    $census.ExternalSystem = 1
+
+    $parameters = @{
+        Method      = 'POST'
+        Header      = $Headers
+        URI         = $URI.Uri
+        Body        = (convertto-json $Census -depth 10)
+        ContentType = 'application/json'
+        ErrorAction = 'silentlyContinue'
+    }
+
+    $Result = Invoke-RestMethod @Parameters
+
+    if ($Result) {
+        return $Result
+    } else {
+        $parameters
+        throw '[ERROR] - '
+    }
+}
+
+function Update-TruHuEmployees {
+    Param(
+        [String]$FQDN = 'api.truhu.com',
+        $APIToken,
+        $Census,
+        [ValidateScript(
+            {$_ -in ([enum]::GetNames([net.securityprotocoltype]))},
+            ErrorMessage = 'ERROR: TLS version must be supported on system (run [enum]::GetNames([net.securityprotocoltype]) for a valid list)'
+        )]
+        [string]$TLSVersion = 'Tls12'
+    )
+
+    # set TLS version
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::"$TLSVersion"
+    
+    # generate uri for generating token
+    [System.UriBuilder]$URI = ('https://{0}/Census' -f $FQDN)
+
+    $Headers = @{
+        Authorization   = ('Bearer {0}' -f $APIToken.access_token)
+        Accept          = 'application/json'
     }
 
     $Parameters = @{
         Method      = 'POST'
         Header      = $Headers
         URI         = $URI.Uri
-        Body        = (convertto-json $body -depth 10)
+        Body        = (convertto-json $Census -depth 10)
         ContentType = 'application/json'
         ErrorAction = 'silentlyContinue'
     }
 
-    $Result = Invoke-RestMethod @Parameters -verbose
+    $Result = Invoke-RestMethod @Parameters
+
+    if ($Result) {
+        return $Result
+    } else {
+        $parameters
+        throw '[ERROR] - '
+    }
+}
+
+function Clear-TruHuEmployees {
+    Param(
+        [Parameter(Mandatory=$true)]
+        $CompanyID,
+        [Parameter(Mandatory=$true)]
+        $APIToken,
+        [Parameter(Mandatory=$false)]
+        $FQDN = 'api.truhu.com',
+        [ValidateScript(
+            {$_ -in ([enum]::GetNames([net.securityprotocoltype]))},
+            ErrorMessage = 'ERROR: TLS version must be supported on system (run [enum]::GetNames([net.securityprotocoltype]) for a valid list)'
+        )]
+        [string]$TLSVersion = 'Tls12'
+    )
+
+    # set TLS version
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::"$TLSVersion"
+    
+    # generate uri for generating token
+    [System.UriBuilder]$URI = ('https://{0}/Census' -f $FQDN)
+
+    $query = [System.Web.HttpUtility]::ParseQueryString('')
+    $query.add('guid',$companyid)
+    $URI.Query = $Query.ToString()
+
+    $Headers = @{
+        Authorization   = ('Bearer {0}' -f $APIToken.access_token)
+        Accept          = 'application/json'
+    }
+
+    $Parameters = @{
+        Method      = 'DELETE'
+        Header      = $Headers
+        URI         = $URI.Uri
+        ContentType = 'application/json'
+        ErrorAction = 'silentlyContinue'
+    }
+
+    $Result = Invoke-RestMethod @Parameters
 
     if ($Result) {
         return $Result
