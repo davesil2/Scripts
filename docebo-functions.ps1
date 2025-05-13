0360080def4cc5e6068f766e9a56624ed61e1b5f
David Silberhorn <davesil2@hotmail.com>
2025-05-12 20:43:34 -0500

    Updates/fixes and new functions

--- docebo-functions.ps1
+++ docebo-functions.ps1
@@ -50,6 +50,7 @@ function Get-DoceboAuthToken {
         URI         = $URI.Uri
         Body        = $Body
         ErrorAction = 'SilentlyContinue'
+        Verbose     = $false
     }
 
     $Response = Invoke-RestMethod @parameters
@@ -77,6 +78,7 @@ function Get-DoceboUsers {
     param(
         [Parameter(Mandatory=$true)]
         [String]$FQDN,
+        [parameter(Mandatory=$true)]
         [string]$Token,
         [switch]$ActiveUsers,
         [string]$SearchText
@@ -108,6 +110,7 @@ function Get-DoceboUsers {
         Headers     = $Headers
         Body        = $Body
         ErrorAction = 'SilentlyContinue'
+        Verbose     = $false
     }
 
      $response = Invoke-RestMethod @parameters
@@ -155,6 +158,7 @@ function Get-DoceboUserDetails {
         URI         = $URI.Uri
         Headers     = $headers
         ErrorAction = 'SilentlyContinue'
+        Verbose     = $false
     }
 
     Write-Verbose ('[INFO] - URI for Query {0}' -f $uri.Uri)
@@ -222,6 +226,7 @@ function Get-DoceboBranches {
         ErrorAction = 'SilentlyContinue'
         Headers     = $Headers
         Body        = $body
+        Verbose     = $false
     }
 
     Write-Verbose ('[INFO] - URI for Query {0}' -f $uri.Uri)
@@ -233,6 +238,20 @@ function Get-DoceboBranches {
     }
 }
 
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.PARAMETER FQDN
+
+.PARAMETER Token
+
+.PARAMETER UserID
+
+#>
 function Disable-DoceboUser {
     param(
         [parameter(Mandatory=$true)]
@@ -265,6 +284,7 @@ function Disable-DoceboUser {
         Headers     = $headers
         Body        = ([system.Text.Encoding]::UTF8.GetBytes(($body | ConvertTo-Json)))
         ErrorAction = 'SilentlyContinue'
+        Verbose     = $false
     }
 
     $response = Invoke-RestMethod @parameters
@@ -274,6 +294,58 @@ function Disable-DoceboUser {
     }
 }
 
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.PARAMETER FQDN
+
+.PARAMETER Token
+
+.PARAMETER UserID
+
+.PARAMETER FirstName
+
+.PARAMETER LastName
+
+.PARAMETER Email
+
+.PARAMETER ExpirationDate
+
+.PARAMETER Role
+
+.PARAMETER Manager
+
+.PARAMETER ManagerID
+
+.PARAMETER BranchID
+
+.PARAMETER Location
+
+.PARAMETER Department
+
+.PARAMETER Division
+
+.PARAMETER Title
+
+.PARAMETER Function
+
+.PARAMETER EmploymentType
+
+.PARAMETER OvertimeStatus
+
+.PARAMETER HireDate
+
+.PARAMETER EmployeeNumber
+
+.PARAMETER AssociateID
+
+.PARAMETER EmployeeID
+
+#>
 function Update-DoceboUser {
     param(
         [parameter(Mandatory=$true)]
@@ -299,75 +371,100 @@ function Update-DoceboUser {
         [String]$EmploymentType,
         [String]$OvertimeStatus,
         [DateOnly]$HireDate,
-        [String]$DisplayName,
         [String]$EmployeeNumber,
         [String]$AssociateID,
         [String]$EmployeeID
     )
+
+    Write-Verbose ('[INFO] - Looking for User [{0}]' -f $DoceboUser.user_id)
 
     $DoceboUser = Get-DoceboUserDetails `
         -FQDN $FQDN `
         -Token $Token `
         -UserID $UserID
 
-    $body = @()
+    $DoceboUser = Get-DoceboUsers `
+        -FQDN $FQDN `
+        -Token $Token `
+        -SearchText $DoceboUser.username
+
+    $body = [pscustomobject]@{}
     $additional_fields = @()
 
     if ($DoceboUser) {
+        Write-Verbose ('[INFO] - Found Docebo User [{0}]' -f $DoceboUser.username)
+
         If ($DoceboUser.first_name -ne $FirstName) {
-            $body += [pscustomobject]@{first_name = $FirstName}
+            $body | Add-Member -Name 'firstname' -Value $FirstName -MemberType NoteProperty
+            Write-Verbose ('[INFO] - First Name in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.first_name,$FirstName)
         }
         if ($DoceboUser.last_name -ne $LastName) {
-            $body += [pscustomobject]@{last_name = $LastName}
+            $body | Add-Member -Name 'lastname' -Value $LastName -MemberType NoteProperty
+            Write-Verbose ('[INFO] - Last Name in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.last_name,$LastName)
         }
         if ($DoceboUser.Email -ne $Email) {
-            $body += [pscustomobject]@{email = $Email}
+            $body | Add-Member -Name 'email' -Value $Email -MemberType NoteProperty
+            Write-Verbose ('[INFO] - Email in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.email,$Email)
         }
         if ($DoceboUser.Expiration_Date -ne $ExpirationDate) {
-            $body += [pscustomobject]@{expiration_date = $ExpirationDate}
+            $body | Add-Member -Name 'expiration_date' -Value $ExpirationDate -MemberType NoteProperty
+            Write-Verbose ('[INFO] - Expiration Date in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.expiration_date,$ExpirationDate)
         }
         if ($DoceboUser.field_1 -ne $Department) {
             $additional_fields += [pscustomobject]@{id = 1; value = $Department}
+            Write-Verbose ('[INFO] - Department in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.Department,$Department)
         }
         if ($DoceboUser.field_7 -ne $Location) {
             $additional_fields += [pscustomobject]@{id = 7; value = $Location}
+            Write-Verbose ('[INFO] - Location in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_7,$Location)
         }
         if ($DoceboUser.field_8 -ne $Division) {
             $additional_fields += [pscustomobject]@{id = 8; value = $Division}
+            Write-Verbose ('[INFO] - Division in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_8,$Division)
         }
         if ($DoceboUser.field_9 -ne $Title) {
             $additional_fields += [pscustomobject]@{id = 9; value = $Title}
+            Write-Verbose ('[INFO] - Title in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.Title,$Title)
         }
         if ($DoceboUser.field_10 -ne $Function) {
             $additional_fields += [pscustomobject]@{id = 10; value = $Function}
+            Write-Verbose ('[INFO] - Function in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_10,$Function)
         }
         if ($DoceboUser.field_11 -ne $EmploymentType) {
             $additional_fields += [pscustomobject]@{id = 11; value = $EmploymentType}
+            Write-Verbose ('[INFO] - Employment Type in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_11,$EmploymentType)
         }
         if ($DoceboUser.field_12 -ne $OvertimeStatus) {
             $additional_fields += [pscustomobject]@{id = 12; value = $OvertimeStatus}
+            Write-Verbose ('[INFO] - Overtime Status in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_12,$OvertimeStatus)
         }
-        if ($DoceboUser.field_13 -ne $DisplayName) {
-            $additional_fields += [pscustomobject]@{id = 13; value = $DisplayName}
+        if ($DoceboUser.field_13 -ne $Manager) {
+            $additional_fields += [pscustomobject]@{id = 13; value = $ADManager.DisplayName}
+            Write-Verbose ('[INFO] - Manager Display Name in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_13,$Manager)
         }
         if ($DoceboUser.field_15 -ne $EmployeeNumber) {
             $additional_fields += [pscustomobject]@{id = 15; value = $EmployeeNumber}
+            Write-Verbose ('[INFO] - Employee Number in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_15,$EmployeeNumber)
         }
         if ($DoceboUser.field_16 -ne $AssociateID) {
             $additional_fields += [pscustomobject]@{id = 16; value = $AssociateID}
+            Write-Verbose ('[INFO] - Associate ID in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_16,$AssociateID)
         }
         if ($DoceboUser.field_17 -ne $EmployeeID) {
             $additional_fields += [pscustomobject]@{id = 17; value = $EmployeeID}
+            Write-Verbose ('[INFO] - Employee ID in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_17,$EmployeeID)
         }
-        if ($DoceboUser.field_19 -ne $ManagerName) {
-            $additional_fields += [pscustomobject]@{id = 19; value = $ManagerName}
+        if ($DoceboUser.field_19 -ne $HireDate) {
+            $additional_fields += [pscustomobject]@{id = 19; value = $HireDate}
+            Write-Verbose ('[INFO] - Manager Name in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.field_19,$HireDate)
         }
-        if ($DoceboUser.manager_names.1 -ne $managerID) {
-            $body += [pscustomobject]@{manager = [pscustomobject]@{1 = $managerID}}
+        if ($DoceboUser.manager_names.'1'.manager_id -ne $managerID) {
+            $body | Add-Member -Name manager -Value ([pscustomobject]@{'1' = $managerId}) -MemberType NoteProperty
+            Write-Verbose ('[INFO] - Manager ID in [Docebo: {0}] does not match provided [{1}]' -f $DoceboUser.manager_names.'1'.manager_id,$ManagerID)
         }
     }
 
-    if ($body -or $additional_fields) {
+    if ((($body.psobject.Members | ?{$_.membertype -eq 'noteproperty'}).count -gt 1) -or $additional_fields) {
         # build uri string
         [System.UriBuilder]$URI = ('https://{0}/manage/v1/user/{1}' -f $FQDN,$UserID)
 
@@ -379,30 +476,58 @@ function Update-DoceboUser {
 
         # generate full body if other fields are updated
         if ($additional_fields) {
-            $body += [pscustomobject]@{additiona_fields = $additional_fields}
+            $body | Add-Member -Name additional_fields -value $additional_fields -MemberType NoteProperty
         }
+
+        Write-Verbose ('[INFO] - Update Info for Body [{0}]' -f ($body | out-string))
 
         # assign parameters
         $parameters = @{
             Method      = 'PUT'
             URI         = $URI.Uri
             Headers     = $headers
-            Body        = ([System.Text.Encoding]::UTF8.GetBytes(($body | ConvertTo-Json)))
+            Body        = ($body | ConvertTo-Json)
             ErrorAction = 'silentlyContinue'
+            ContentType = 'application/json'
+            Verbose     = $false
         }
 
         $response = Invoke-RestMethod @parameters
 
         if ($response) {
-            return $response
+            #return $response
+            return @{Changes=$body;Success=$response.data.success}
         }
+    } else {
+        return @{Changes='';Success=$true}
     }
 }
 
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.PARAMETER FQDN
+
+.PARAMETER Token
+
+.PARAMETER BranchID
+
+.PARAMETER UseSecondaryID
+
+.PARAMETER IncludePath
+
+#>
 function Get-DoceboBranchDetails {
     param(
+        [parameter(Mandatory=$true)]
         [String]$FQDN,
+        [parameter(Mandatory=$true)]
         [String]$Token,
+        [parameter(Mandatory=$true)]
         [String]$BranchID,
         [Switch]$UseSecondaryID,
         [Switch]$IncludePath
@@ -429,6 +554,7 @@ function Get-DoceboBranchDetails {
         URI         = $URI.Uri
         Headers     = $headers
         ErrorAction = 'Silentlycontinue'
+        Verbose     = $false
     }
 
     $response = Invoke-RestMethod @parameters
@@ -438,11 +564,31 @@ function Get-DoceboBranchDetails {
     }
 }
 
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.PARAMETER FQDN
+
+.PARAMETER Token
+
+.PARAMETER UserID
+
+.PARAMETER BranchID
+
+#>
 function Move-DoceboUserBranch {
     param(
+        [parameter(Mandatory=$true)]
         $FQDN,
+        [parameter(Mandatory=$true)]
         $Token,
+        [parameter(Mandatory=$true)]
         $UserID,
+        [parameter(Mandatory=$true)]
         $BranchID
     )
 
@@ -467,6 +613,7 @@ function Move-DoceboUserBranch {
             Headers     = $headers
             Body        = ($body | convertto-json)
             ErrorAction = 'silentlycontinue'
+            Verbose     = $false
         }
 
         $response = Invoke-RestMethod @parameters
@@ -474,5 +621,62 @@ function Move-DoceboUserBranch {
         if ($response) {
             return $response.success
         }
+    }
+}
+
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.PARAMETER FQDN
+
+.PARAMETER Token
+
+.PARAMETER Code
+
+.PARAMETER ParentID
+
+.PARAMETER translations
+
+#>
+function New-DoceboBranch {
+    param(
+        $FQDN,
+        $Token,
+        $Code,
+        $ParentID,
+        $translations = @{all=$Code}
+    )
+
+    [System.UriBuilder]$URI = ('https://{0}/manage/v1/orgchart' -f $FQDN)
+
+    $headers = @{
+        "Authorization" = ('Bearer {0}' -f $Token)
+        "Content-Type"  = 'application/json'
+    }
+
+    $body = @{
+        code = $Code
+        id_parent = $ParentID
+        translations = $translations
+    }
+
+    $parameters = @{
+        Method      = 'POST'
+        URI         = $URI.Uri
+        Headers     = $headers
+        Body        = ($body | convertto-json)
+        ErrorAction = 'silentlycontinue'
+        ContentType = 'application/json'
+        Verbose     = $false
+    }
+
+    $response = Invoke-RestMethod @parameters
+
+    If ($response) {
+        return $response.data
     }
 }
