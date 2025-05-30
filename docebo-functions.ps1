5d3bd5d2591a7081bc6e89f91c46650cb7f961ad
David Silberhorn <davesil2@hotmail.com>
2025-05-29 22:05:52 -0400

    hiredate fix again

--- docebo-functions.ps1
+++ docebo-functions.ps1
@@ -453,8 +453,10 @@ function Update-DoceboUser {
         Write-Verbose ('[INFO] - Found Docebo User [{0}]' -f $DoceboUser.username)
 
         try {
+            write-verbose ('hire date is [{0}]' -f $hiredate.tostring('yyyy-MM-dd'))
             if ($HireDate) {
                 $DoceboHireDate = ([System.DateOnly]$DoceboUser.field_19)
+                write-verbose ('Docebo Hire Date is [{0}]' -f $DoceboHireDate.tostring('yyyy-MM-dd'))
                 if ($DoceboHireDate -ne $HireDate -or -Not $DoceboHireDate) {
                     $additional_fields += [pscustomobject]@{id = 19; value = $HireDate.ToString('yyyy-MM-dd')}
                     Write-Verbose ('[INFO] - Manager Name in [Docebo: {0}] does not match provided [{1}]' -f $DoceboHireDate.ToString('yyyy-MM-dd'),$HireDate.ToString('yyyy-MM-dd'))
