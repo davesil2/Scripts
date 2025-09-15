5187b130b46328ae119d82dfb5f59ed60f01845e
David Silberhorn <you@example.com>
2025-09-15 17:17:32 -0500

    Add Fields List Function and updates to documentation of functions

--- bamboohr-functions.ps1
+++ bamboohr-functions.ps1
@@ -1,60 +1,60 @@
 <#
-    .SYNOPSIS
+.SYNOPSIS
 
-    Provides output of Users from BambooHR REST API
+Provides output of Users from BambooHR REST API
 
-    .DESCRIPTION
+.DESCRIPTION
 
-    Using BambooHR's provide developer API, this function allows a quick query against the specific BambooHR Domain when used with the API Key.  The function allows customization of output based on desired results
+Using BambooHR's provide developer API, this function allows a quick query against the specific BambooHR Domain when used with the API Key.  The function allows customization of output based on desired results
 
-    .PARAMETER CompanyDomain
+.PARAMETER CompanyDomain
 
-    This is the domain provided by BambooHR for your unique instance. Typically this would be the company name in the first part of the url <company>.bamboohr.com
+This is the domain provided by BambooHR for your unique instance. Typically this would be the company name in the first part of the url <company>.bamboohr.com
 
-    .PARAMETER OnlyCurrent
+.PARAMETER OnlyCurrent
 
-    This parameter will ensure only current users are returned in the output.
+This parameter will ensure only current users are returned in the output.
 
-    .PARAMETER Fields
+.PARAMETER Fields
 
-    This is an array list of the fields that should be returned as part of the query.  The default list includes:
+This is an array list of the fields that should be returned as part of the query.  The default list includes:
 
-        "firstname",
-        "lastname",
-        "id",
-        "employeenumber",
-        "preferredname",
-        "customPreferredLastName",
-        "workemail",
-        "employmentHistoryStatus",
-        "jobtitle",
-        "supervisoremail",
-        "department",
-        "division",
-        "manager",
-        "location",
-        "hiredate",
-        "workPhone",
-        "mobilePhone",
-        "country",
-        "customBusinessUnitCode",
-        "status",
-        "customPositionID",
-        "customFunction",
-        "Exempt",
-        "terminationDate"
+    "firstname",
+    "lastname",
+    "id",
+    "employeenumber",
+    "preferredname",
+    "customPreferredLastName",
+    "workemail",
+    "employmentHistoryStatus",
+    "jobtitle",
+    "supervisoremail",
+    "department",
+    "division",
+    "manager",
+    "location",
+    "hiredate",
+    "workPhone",
+    "mobilePhone",
+    "country",
+    "customBusinessUnitCode",
+    "status",
+    "customPositionID",
+    "customFunction",
+    "Exempt",
+    "terminationDate"
 
-    BambooHR provides a list of valid values - https://documentation.bamboohr.com/docs/list-of-field-names
+BambooHR provides a list of valid values - https://documentation.bamboohr.com/docs/list-of-field-names
 
-    .PARAMETER APIKey
+.PARAMETER APIKey
 
-    This is the API Key generated under the user account or in the admin portal for access to data.
+This is the API Key generated under the user account or in the admin portal for access to data.
 
-    .EXAMPLE
+.EXAMPLE
 
-    Get-BambooHRUsers -Domain companydomain -APIKey 123456678900
+Get-BambooHRUsers -CompanyDomain companydomain -APIKey 123456678900
 
-    # Returns the users from the BambooHR company company.bamboohr.com with the APIKey
+# Returns the users from the BambooHR company company.bamboohr.com with the APIKey
 
 #>
 function Get-BambooHRUsers {
@@ -74,15 +74,19 @@ function Get-BambooHRUsers {
         [string]$APIKey
     )
 
+    # Build URL String
     [System.UriBuilder]$URI = ("https://api.bamboohr.com/api/gateway.php/{0}/v1/reports/custom?format=csv&onlyCurrent={1}" -f $CompanyDomain,$OnlyCurrent.IsPresent.ToString().tolower())
 
+    # Build body for fields to return
     $Body = [pscustomobject]@{fields=$fields} | ConvertTo-Json
 
+    # build header information
     $Headers = @{
         "content-type"  = "application/json"
         "authorization" = ("Basic {0}" -f [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($APIKey)`:x")))
     }
 
+    # build web request
     $Splat = @{
         Uri         = $URI.uri
         Method      = 'Post'
@@ -92,14 +96,16 @@ function Get-BambooHRUsers {
         Verbose     = $false
     }
 
-    $Result = Invoke-RestMethod @Splat -ErrorAction SilentlyContinue
+    # execute web request
+    $response = Invoke-RestMethod @Splat -ErrorAction SilentlyContinue
 
-    if ($result) {
-        Return ($result | ConvertFrom-Csv)
+    if ($response) {
+        Return ($response | ConvertFrom-Csv)
     } else {
         throw 'Either no results returned or access denied!'
     }
 }
+
 <#
 .SYNOPSIS
 This function uses BambooHR API to retrieve the users thumbnail photo profile picture.
@@ -160,6 +166,67 @@ function Get-BambooHRUserPhoto {
     # return content if it exists
     if ($response.Content) {
         return @{PhotoThumbprint = $response.Content}
+    } else {
+        throw 'Either no results returned or access denied'
+    }
+}
+
+<#
+.SYNOPSIS
+
+This function retrieves the list of fields, their alias and type from BambooHR
+
+.DESCRIPTION
+
+.EXAMPLE
+
+Get-BambooHRFieldsList -CompanyDomain "companydomain" -APIKey "1234123454325432"
+
+# Returns fields and alias from the BambooHR instance
+
+.PARAMETER CompanyDomain
+
+This is the domain provided by BambooHR for your unique instance. Typically this would be the company name in the first part of the url <company>.bamboohr.com
+
+.PARAMETER APIKey
+
+This is the API Key generated under the user account or in the admin portal for access to data.
+
+#>
+function Get-BambooHRFieldsList {
+    param(
+        [parameter(Mandatory=$true)]
+        [Alias("Domain")]
+        [string]$CompanyDomain,
+        [string]$APIKey
+    )
+
+    # Build URL String
+    [system.Uribuilder]$URI = ('https://{0}.bamboohr.com/api/v1/meta/tables' -f $CompanyDomain)
+
+    # build header information
+    $Headers = @{
+        accept          = 'application/json'
+        'content-type'  = 'application/json'
+        'authorization' = ("Basic {0}" -f [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($APIKey)`:x")))
+    }
+
+    # build web request
+    $Splat = @{
+        ErrorAction     = 'SilentlyContinue'
+        Method          = 'Get'
+        Uri             = $URI.Uri
+        Headers         = $Headers
+    }
+
+    # execute web request
+    try {
+        $response = Invoke-WebRequest @Splat
+    } catch {}
+    
+    # return content if it exists
+    if ($response.Content) {
+        return ($response.Content | ConvertFrom-Json)
     } else {
         throw 'Either no results returned or access denied'
     }
